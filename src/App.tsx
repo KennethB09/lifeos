79cabfac23c359729e7500e07dc9d4b52ce06e46
Kenneth Bacaltos <kennethbacaltos091@gmail.com>
2025-05-31 08:50:56 +0800

    css

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="text-teal-600">
+      <p className="text-red-800">
         Click on the Vite and React logos to learn more
       </p>
     </>
